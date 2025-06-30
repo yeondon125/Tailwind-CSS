65a761b0615bff9f355b491c912c0aaeaf0929d8
yeondon <s25030@gsm.hs.kr>
2025-06-30 23:48:37 +0900

    div 나누기

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-};
-
-// tailwind.config.js
-module.exports = {
-  content: ["./index.html"], // 혹은 ./src/**/*.html 등
-  theme: {
     extend: {
       colors: {
-        darkgray: "#1e1f20", // 원하는 이름으로 등록
+        darkgray: "#1e1f20",
+        bl: "#0E0E0E", // 원하는 이름으로 등록
       },
     },
   },
