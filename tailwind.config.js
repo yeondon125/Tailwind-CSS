16ee3c9b2147beb365d63138a92c6c29e5785d18
yeondon <s25030@gsm.hs.kr>
2025-07-03 00:02:43 +0900

    오른쪽 요소 버튼 미완

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
         darkgray: "#1e1f20",
         bl: "#0E0E0E", // 원하는 이름으로 등록
         ap: "#A8C7FA",
+        c4: "#c4c7c5",
+      },
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
       },
     },
   },
